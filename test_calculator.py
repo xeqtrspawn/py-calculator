dbaceb01dcf4452af7bae5b268138677ebd66ccf
Abhishek Mishra <abhishekmishra@Abhisheks-MacBook-Pro.local>
2018-12-23 13:24:29 +0100

    Added test case for division

--- test_calculator.py
+++ test_calculator.py
@@ -15,3 +15,7 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
+
+    def test_division(self):
+        assert 10 == calculator.divide(100, 10)
+
